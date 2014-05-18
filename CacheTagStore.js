dcaf063eed7903a275c44fcd77ea30ddc8b685d9
Michael Clark <mjac@mjac.co.uk>
2014-05-18 09:28:23 +0100

    Split up tag store into multiple files

---
+++ CacheTagStore.js
@@ -0,0 +1,39 @@
+function CacheTagStore(sourceTagStore, storage)
+{
+	this.sourceTagStore = sourceTagStore;
+
+	// localstorage
+	this.storage = storage;
+}
+
+CacheTagStore.prototype = {
+	RequestTags: function (bookmarkUrl, callback)
+	{
+		var tags;
+
+		if (this.url in localStorage) {
+			var tagJson = this.storage[this.url];
+
+			tags = JSON.parse(tagJson).map(function (tag) {
+				return tag;
+			});
+
+			callback(bookmarkUrl, tags);
+
+			return;
+		} 
+
+		bookmark.requestTags(function (tags) {
+			this.storage[bookmarkUrl] = JSON.stringify(tags);
+
+			var urlMatch = bookmarkUrl.match(/\/\/(?:www\.)?([^/]+)/);
+
+			if (urlMatch !== null) {
+				tags.push(urlMatch[1]);
+			}
+
+			callback(bookmarkUrl, tags);
+		}.bind(this));
+	}
+}
+
