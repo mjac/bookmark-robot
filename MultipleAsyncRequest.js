03ce816c40b9764c6dff096ae3d6272213cb4733
Michael Clark <mjac@mjac.co.uk>
2014-09-22 09:08:41 +0100

    Add class for performing multiple async requests (can promises support this structure)

---
+++ MultipleAsyncRequest.js
@@ -0,0 +1,30 @@
+function MultipleAsyncRequest()
+{
+	this.finalCallback = null;
+	this.requests = [];
+}
+
+MultipleAsyncRequest.prototype = {
+	AddRequest: function (request) {
+		this.requests.push(request);
+	},
+
+	SetFinalCallback: function (finalCallback) {
+		this.finalCallback = finalCallback;
+	},
+
+	Execute: function () {
+		var finalCallback = this.finalCallback;
+		var requestsRemaining = requests.length;
+		
+		this.requests.forEach(function (request) {
+			request(function () {
+				requestsRemaining--;
+				
+				if (requestsRemaining < 1 && finalCallback) {
+					finalCallback();
+				}
+			});
+		});
+	}
+};
