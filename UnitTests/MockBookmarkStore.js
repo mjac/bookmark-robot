c127f8b0e143641eb560ef4d13176ea40c6db154
Michael Clark <mjac@mjac.co.uk>
2014-10-19 15:19:17 +0100

    Add mock bookmark store for unit tests

---
+++ UnitTests/MockBookmarkStore.js
@@ -0,0 +1,12 @@
+
+test('', function () {
+	var input = [
+		'Coding/C++ Tips',
+		'Cooking/How to cook bolognese',
+	];
+
+	var mockBookmarkStore = new MockBookmarkStore();
+
+	var bookmarks = mockBookmarkStore.GetBookmarks(input);
+	
+});
