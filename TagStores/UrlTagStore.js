b3eefd4b236fde49af733a3cf3125d8f0f3cb24c
Michael Clark <michael.clark@red-gate.com>
2015-03-17 11:06:35 +0000

    Add tests and logic to UrlTagStore to support additional log formats

--- TagStores/UrlTagStore.js
+++ TagStores/UrlTagStore.js
@@ -7,10 +7,19 @@ UrlTagStore.prototype = {
 	{
 		var tags = [];
 		
-		var urlMatch = bookmarkUrl.match(/\/\/(?:www\.)?([^/]+)/);
+		var urlMatch = bookmarkUrl.match(/\/\/(?:www\.)?([^/]+)/i);
 		
 		if (urlMatch !== null) {
-			tags.push(urlMatch[1]);
+            var hostname = urlMatch[1].toLowerCase();
+            
+            if (hostname.indexOf('.') >= 0) {
+                // Fully qualified hostname
+                tags.push(hostname);
+            } else if (hostname === 'localhost') {
+                tags.push('localhost');
+            } else {
+                tags.push('intranet');
+            }
 		}
 
 		callback(bookmarkUrl, tags);
--- UnitTests/UrlTagStoreTests.js
+++ UnitTests/UrlTagStoreTests.js
@@ -2,11 +2,28 @@ require(['TagStores/UrlTagStore'], function () {
     var tagStore = new UrlTagStore();
     
 	QUnit.test('Tag is URL hostname', function (assert) {
+        testSingleTag('http://google.com/', 'google.com', assert);
+	});
+    
+	QUnit.test('Tag is normalised to lowercase', function (assert) {
+        testSingleTag('http://GOOGLE.COM/', 'google.com', assert);
+	});
+    
+	QUnit.test('Localhost maps to own tag', function (assert) {
+        testSingleTag('http://localhost/', 'localhost', assert);
+	});
+    
+	QUnit.test('Local hostname maps to intranet', function (assert) {
+        testSingleTag('http://teamcity/', 'intranet', assert);
+	});
+    
+    function testSingleTag(url, expected, assert)
+    {
         var done = assert.async();
         
-        tagStore.RequestTags('http://google.com/', function (bookmarkUrl, tags) {
-            assert.deepEqual(tags, ['google.com']);
+        tagStore.RequestTags(url, function (bookmarkUrl, tags) {
+            assert.deepEqual(tags, [expected]);
             done();
         });
-	});
+    }
 });
