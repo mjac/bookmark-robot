f0835c6e8c6778594405b19c2734c5cde4d05d1f
Michael Clark <michael.clark@red-gate.com>
2015-03-17 10:55:39 +0000

    Improve URL Tag test

--- UnitTests/UrlTagStoreTests.js
+++ UnitTests/UrlTagStoreTests.js
@@ -1,11 +1,11 @@
 require(['TagStores/UrlTagStore'], function () {
     var tagStore = new UrlTagStore();
     
-	QUnit.test('Correct bookmark is retrieved from URL', function (assert) {
+	QUnit.test('Tag is URL hostname', function (assert) {
         var done = assert.async();
         
         tagStore.RequestTags('http://google.com/', function (bookmarkUrl, tags) {
-            assert.deepEqual(tags, ['google.com'], "Correct tag should be found");
+            assert.deepEqual(tags, ['google.com']);
             done();
         });
 	});
