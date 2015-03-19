43b625fc03c94c9430f1e7848b2d3493e4318264
Michael Clark <mjac@mjac.co.uk>
2015-03-19 08:40:48 +0000

    Add missing BookmarkTreeReader from last commit

---
+++ BookmarkTreeReader.js
@@ -0,0 +1,30 @@
+define([
+	'Bookmark',
+	'Folder'
+], function (
+	bookmarkConstructor,
+	folderConstructor
+) {
+	function BookmarkTreeReader()
+	{
+	}
+
+	BookmarkTreeReader.prototype.readTree = function (bookmarkTree, bookmarks) {
+		if ('children' in bookmarkTree) {
+			var folder = new folderConstructor(bookmarkTree.title);
+			bookmarks.AddFolder(folder);
+			
+			this.readTree(bookmarkTree.children, folder);
+		} else if ($.isArray(bookmarkTree)) {
+			for (localIdx in bookmarkTree) {
+				var bookmarkChild = bookmarkTree[localIdx];
+				this.readTree(bookmarkChild, bookmarks);
+			}
+		} else {
+			var newBookmark = new bookmarkConstructor(bookmarkTree.title, bookmarkTree.url);
+			bookmarks.AddBookmark(newBookmark);
+		}
+	}
+	
+	return new BookmarkTreeReader();
+});
