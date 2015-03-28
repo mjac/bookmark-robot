46faf7a8e68908fca1ea64c3df0824efd9a8b270
Michael Clark <mjac@mjac.co.uk>
2015-03-28 23:15:13 +0000

    Adapt BookmarkContentRepository to RequireJS

--- BookmarkContentRepository.js
+++ BookmarkContentRepository.js
@@ -1,28 +1,32 @@
-function RemoteBookmarkContentRepository()
-{
-}
-
-RemoteBookmarkContentRepository.prototype = {
-	GetHTML: function (bookmarkUrl, callbackSuccess, callbackFailure) {
-		this.found = null;
-		this.loading = true;
-
-		$.ajax({
-			url: bookmarkUrl, 
-
-			dataType: 'html',
-
-			timeout: 5000,
-
-			success: function (data, textStatus, jqXHR) {
-				callbackSuccess(data);
-			},
-
-			error: function (jqXHR, textStatus, errorThrown) {
-				if (callbackFailure) {
-					callbackFailure();
-				}
-			}
-		});
+define(function () {
+	function RemoteBookmarkContentRepository()
+	{
 	}
-};
+
+	RemoteBookmarkContentRepository.prototype = {
+		GetHTML: function (bookmarkUrl, callbackSuccess, callbackFailure) {
+			this.found = null;
+			this.loading = true;
+
+			$.ajax({
+				url: bookmarkUrl, 
+
+				dataType: 'html',
+
+				timeout: 5000,
+
+				success: function (data, textStatus, jqXHR) {
+					callbackSuccess(data);
+				},
+
+				error: function (jqXHR, textStatus, errorThrown) {
+					if (callbackFailure) {
+						callbackFailure();
+					}
+				}
+			});
+		}
+	};
+
+	return new RemoteBookmarkContentRepository();
+});
