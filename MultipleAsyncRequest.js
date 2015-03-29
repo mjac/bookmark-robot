388f24f79103b13477a76e26691911fe9ea03112
Michael Clark <mjac@mjac.co.uk>
2015-03-29 11:43:17 +0100

    Adapt MultipleAsyncRequest to allow throttling

--- MultipleAsyncRequest.js
+++ MultipleAsyncRequest.js
@@ -3,6 +3,9 @@ define(function () {
     {
         this.finalCallback = null;
         this.requests = [];
+
+		this.pendingRequests = 0;
+		this.maxPendingRequests = null;
     }
 
     MultipleAsyncRequest.prototype = {
@@ -14,19 +17,42 @@ define(function () {
             this.finalCallback = finalCallback;
         },
 
+		SetMaxPendingRequests: function (maxPendingRequests) {
+			this.maxPendingRequests = maxPendingRequests;
+		},
+
         Execute: function () {
+			this.nextRequestIndex = 0;
+			this.totalRequests = this.requests.length;
+
+			this.pendingRequests = 0;
+
+			this._ProcessRequests();
+		},
+
+		_ProcessRequests: function () {
             var finalCallback = this.finalCallback;
             var requestsRemaining = this.requests.length;
-            
-            this.requests.forEach(function (request) {
-                request(function () {
-                    requestsRemaining--;
-                    
-                    if (requestsRemaining < 1 && finalCallback) {
-                        finalCallback();
-                    }
-                });
-            });
+
+			while (this.nextRequestIndex < this.totalRequests) {
+				if (this.maxPendingRequests !== null && this.pendingRequests >= this.maxPendingRequests) {
+					return;
+				}
+
+				var request = this.requests[this.nextRequestIndex];
+
+				this.pendingRequests++;
+				this.nextRequestIndex++;
+
+				request(function () {
+					this.pendingRequests--;
+					this._ProcessRequests();
+				}.bind(this));
+			}
+
+			if (this.pendingRequests < 1 && finalCallback) {
+				finalCallback();
+			}
         }
     };
     
