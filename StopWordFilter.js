761aab17278e90bb67af64b672a6380db77e8bb8
Michael Clark <mjac@mjac.co.uk>
2015-04-02 21:05:52 +0100

    Add 'us' to StopWordFilter

--- StopWordFilter.js
+++ StopWordFilter.js
@@ -36,7 +36,7 @@ define(function () {
 
 	// Common title words that convey no meaning
 	stopWords += ',';
-	stopWords += 'web,website,site,net,welcome,blog,best,big,day,use,ways,way,org,file,magazine,uk,great,good,things,open,online,free,developer,developers,think,world,write,login,redirecting';
+	stopWords += 'web,website,site,net,welcome,blog,best,big,day,use,ways,way,org,file,magazine,uk,great,good,things,open,online,free,developer,developers,think,world,write,login,redirecting,us';
 
 	return new StopWordFilter();
 });
