b6f858c856a9e1d68f41c41a6f7d358b67844d8e
Michael Clark <mjac@mjac.co.uk>
2015-04-05 14:59:29 +0100

    Add Knockout

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 var require = {
 	paths: {
 		"jquery": "vendor/jquery/dist/jquery.min",
-		"jquery.jstree": "vendor/jstree/dist/jstree.min"
+		"jquery.jstree": "vendor/jstree/dist/jstree.min",
+		"knockout": "vendor/knockout/dist/knockout"
 	}
 };
